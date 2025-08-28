aa50977447fbf0bcf54151dc48dd1693f3f80519
Halil Ibrahim Balik <halilbalik2101@gmail.com>
2025-08-28 15:13:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Kriptografi servislerini ekle
 builder.Services.AddScoped<Bilgi_Güvenliği_ve_Kriptoloji.Services.IAESService, Bilgi_Güvenliği_ve_Kriptoloji.Services.AESService>();
 builder.Services.AddScoped<Bilgi_Güvenliği_ve_Kriptoloji.Services.IRSAService, Bilgi_Güvenliği_ve_Kriptoloji.Services.RSAService>();
 builder.Services.AddScoped<Bilgi_Güvenliği_ve_Kriptoloji.Services.ISHAService, Bilgi_Güvenliği_ve_Kriptoloji.Services.SHAService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
